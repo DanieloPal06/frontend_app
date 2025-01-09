d42ddfbc80952cb0fc2fb9b0d4899ca6cd91fa60
Daniel Santamaría Álvarez <d.santamaria@uniandes.edu.co>
2025-01-09 19:21:12 +0000

    logo and headers updated

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import Link from 'next/link';
+import Image from 'next/image';
 import { usePathname } from 'next/navigation';
 import { Button } from '@/components/ui/button';
 import { ModeToggle } from '@/components/mode-toggle';
@@ -10,12 +11,14 @@ const Header = () => {
 
   return (
     <header className="bg-background border-b">
-      <div className="container mx-auto px-4 py-4 flex justify-between items-center">
-        <Link href="/" className="text-2xl font-bold">
-          PredictPal
+      <div className="container mx-auto px-5 py-2 flex flex-col md:flex-row justify-between items-center">
+        <Link href="/" className="w-full md:w-1/4 mb-4 md:mb-0">
+          <div className="bg-white border-2 border-white rounded-lg p-2 flex items-center justify-center w-full md:w-[150px] h-[100px]">
+            <Image src="/logo.png" alt="Logo" width={120} height={120} />
+          </div>
         </Link>
-        <nav>
-          <ul className="flex space-x-4">
+        <nav className="w-full md:w-auto">
+          <ul className="flex flex-row space-x-4 justify-between">
             <li>
               <Link href="/" passHref>
                 <Button variant={pathname === '/' ? 'default' : 'ghost'}>Home</Button>
@@ -31,18 +34,18 @@ const Header = () => {
                 <Button variant={pathname === '/courses' ? 'default' : 'ghost'}>Courses</Button>
               </Link>
             </li>
-            <li>
+            {/* <li>
               <Link href="/subscription" passHref>
                 <Button variant={pathname === '/subscription' ? 'default' : 'ghost'}>Subscribe</Button>
               </Link>
-            </li>
+            </li> */}
           </ul>
         </nav>
-        <div className="flex items-center space-x-4">
+        <div className="flex items-center space-x-4 w-full md:w-1/4 justify-end mt-4 md:mt-0">
           <ModeToggle />
-          <Link href="/login" passHref>
+          {/* <Link href="/login" passHref>
             <Button>Login</Button>
-          </Link>
+          </Link> */}
         </div>
       </div>
     </header>
