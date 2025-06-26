51ddba2a413595e579860a5e30b46ddaf31ce55c
Daniel “Danielo06” Santamaría Álvarez <danielsantamaria074@gmail.com>
2025-06-26 10:40:11 +0000

    Logo updated in navbar

--- src/components/landing/navbar.tsx
+++ src/components/landing/navbar.tsx
@@ -53,15 +53,7 @@ export default function Navbar({ language, setLanguage, navContent, commonConten
         {/* Left Side */}
         <div className="flex flex-1 justify-start">
           <Link href="/" className="flex items-center space-x-2">
-            <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <defs>
-                  <linearGradient id="logo-gradient" x1="0%" y1="0%" x2="100%" y2="100%">
-                    <stop offset="0%" style={{stopColor: 'hsl(var(--accent))', stopOpacity:1}} />
-                    <stop offset="100%" style={{stopColor: 'hsl(var(--primary))', stopOpacity:1}} />
-                  </linearGradient>
-                </defs>
-                <path d="M16 0C7.163 0 0 7.163 0 16C0 24.837 7.163 32 16 32C24.837 32 32 24.837 32 16C32 7.163 24.837 0 16 0ZM23.237 20.363L18.875 16L23.237 11.637C23.55 11.325 23.55 10.812 23.237 10.5L21.5 8.763C21.188 8.45 20.675 8.45 20.363 8.763L16 13.125L11.637 8.763C11.325 8.45 10.812 8.45 10.5 8.763L8.763 10.5C8.45 10.812 8.45 11.325 8.763 11.637L13.125 16L8.763 20.363C8.45 20.675 8.45 21.188 8.763 21.5L10.5 23.237C10.812 23.55 11.325 23.55 11.637 23.237L16 18.875L20.363 23.237C20.675 23.55 21.188 23.55 21.5 23.237L23.237 21.5C23.55 21.188 23.55 20.675 23.237 20.363Z" fill="url(#logo-gradient)"/>
-            </svg>
+            <img src="PredictPal.png" alt="PredictPal Logo" className="h-8 w-auto"/>
             <h1 className="font-headline text-2xl font-bold">
               <span className="text-accent">Predict</span><span className="text-primary">Pal</span>
             </h1>
